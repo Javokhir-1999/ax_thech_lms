b9b6ee1897469e7fbed6b077ff41ca2809e88e2f
Java Fox <1810214ja@gmail.com>
2024-01-17 16:58:42 +0500

    async driver - psycopg

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+import asyncio
 from app.api_routers import router
 
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 app = FastAPI()
 app.include_router(router)
 
