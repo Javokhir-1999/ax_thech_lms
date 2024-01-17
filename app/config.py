964b338173c112b656f8ff8c7cd77228ee4d2d7c
Java Fox <1810214ja@gmail.com>
2024-01-17 16:53:35 +0500

    async driver - psycopg

--- app/config.py
+++ app/config.py
@@ -4,18 +4,18 @@ class Config:
     """Base configuration class (default configuration settings)."""
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:postgres1999@localhost:5432/lms_dev')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql+psycopg://postgres:postgres1999@localhost:5432/lms_dev')
 
 class DevelopmentConfig(Config):
     """Development configuration class."""
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('DEV_DATABASE_URL', 'postgresql://postgres:postgres1999@localhost:5432/lms_dev')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DEV_DATABASE_URL', 'postgresql+psycopg://postgres:postgres1999@localhost:5432/lms_dev')
 
 class TestingConfig(Config):
     """Testing configuration class."""
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URL', 'postgresql://postgres:postgres1999@localhost:5432/lms_test')
+    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URL', 'postgresql+psycopg://postgres:postgres1999@localhost:5432/lms_test')
 
 class ProductionConfig(Config):
     """Production configuration class."""
-    SQLALCHEMY_DATABASE_URI = os.getenv('PROD_DATABASE_URL', 'postgresql://postgres:postgres1999@localhost:5432/lms_prod')
+    SQLALCHEMY_DATABASE_URI = os.getenv('PROD_DATABASE_URL', 'postgresql+psycopg://postgres:postgres1999@localhost:5432/lms_prod')
